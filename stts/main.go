e9681fdee9d16ecc9e94a11fdfb7d1839c576793
h1x <h1x@linuxconsole.net>
2023-04-21 19:35:03 +0200

    move sysinfo functions to a seperate file
    benchmark result: mean 285433 (stdev 4.90%)

--- stts/main.go
+++ stts/main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
 	"os"
-	"strconv"
-	"strings"
-	"syscall"
 	"time"
 )
 
@@ -63,80 +59,6 @@ func getVars(vars *varsT) {
 	var err error
 	vars.meminfoFD, err = os.Open("/proc/meminfo")
 	errExit(err)
-}
-
-func getSysinfo(st *sttsT, vars *varsT) {
-	si := &syscall.Sysinfo_t{}
-	_ = syscall.Sysinfo(si)
-
-	uptimeTime := time.Unix(si.Uptime, 0)
-	st.uptime = uptimeTime.Sub(time.Unix(0, 0))
-
-	const SI_LOAD_SHIFT = 16
-	st.loads[0] = float64(si.Loads[0]) / (1 << SI_LOAD_SHIFT)
-	st.loads[1] = float64(si.Loads[1]) / (1 << SI_LOAD_SHIFT)
-	st.loads[2] = float64(si.Loads[2]) / (1 << SI_LOAD_SHIFT)
-
-	st.procs = int(si.Procs)
-
-	st.mem.total = int(si.Totalram)
-	st.mem.free = int(si.Freeram)
-	st.mem.shared = int(si.Sharedram)
-	st.mem.buffer = int(si.Bufferram)
-
-	// add missing info from /proc/meminfo
-	readMeminfo(st, vars)
-	st.mem.used = st.mem.total - st.mem.free - st.mem.buffer - st.mem.cache
-}
-
-func readMeminfo(st *sttsT, vars *varsT) {
-	scanner := bufio.NewScanner(vars.meminfoFD)
-	lineID := 1
-	for scanner.Scan() {
-		switch lineID {
-		case 3:
-			key, val := parseMeminfoLine(scanner.Text())
-			if key != "MemAvailable" {
-				msg := "wrong /proc/meminfo line number: %s"
-				errExit(fmt.Errorf(msg, key))
-			}
-			st.mem.avail = val * 1024
-		case 5:
-			key, val := parseMeminfoLine(scanner.Text())
-			if key != "Cached" {
-				msg := "wrong /proc/meminfo line number: %s"
-				errExit(fmt.Errorf(msg, key))
-			}
-			st.mem.cache = val * 1024
-		case 24:
-			key, val := parseMeminfoLine(scanner.Text())
-			if key != "SReclaimable" {
-				msg := "wrong /proc/meminfo line number: %s"
-				errExit(fmt.Errorf(msg, key))
-			}
-			st.mem.cache += val * 1024
-		case 25:
-			break
-		}
-		lineID++
-	}
-}
-
-func parseMeminfoLine(line string) (string, int) {
-	fields := strings.Split(line, ":")
-	if len(fields) != 2 {
-		errExit(fmt.Errorf("can't parse /proc/meminfo line: %s", line))
-	}
-	key := strings.TrimSpace(fields[0])
-	valStr := strings.TrimSpace(fields[1])
-	valStr = strings.TrimSuffix(valStr, " kB")
-	val, err := strconv.ParseInt(valStr, 10, 64)
-	if err != nil {
-		msg := "can't parse /proc/meminfo line: %s\nerror: %h"
-		errExit(fmt.Errorf(msg, line, err))
-	}
-
-	return key, int(val)
 }
 
 func prettyPrint(st sttsT) {
