904582197eb19d1be8dd97ada5531c51b940c2f2
h1x <h1x@linuxconsole.net>
2023-12-27 15:45:22 +0100

    update airvpn status program

--- airvpn/main.go
+++ airvpn/main.go
@@ -58,20 +58,30 @@ func main() {
 		panic(err)
 	}
 
-	var eu, america []serverT
+	var all, eu, america, asia, oceania []serverT
 
 	for _, s := range info.Servers {
 		s.Location, _, _ = str.Cut(s.Location, ",")
 		s.Location = str.TrimSuffix(s.Location, " City")
 		s.FreeBW = s.BandwithMax - s.Bandwith
 
+			all = append(all, s)
+
 		switch s.Continent {
 		case "Europe":
 			eu = append(eu, s)
 		case "America":
 			america = append(america, s)
+		case "Asia":
+			asia = append(asia, s)
+		case "Oceania":
+			oceania = append(oceania, s)
 		}
 	}
+
+	sort.Slice(all, func(i, j int) bool {
+		return all[i].Load >= all[j].Load
+	})
 
 	sort.Slice(eu, func(i, j int) bool {
 		return eu[i].Load >= eu[j].Load
@@ -81,19 +91,50 @@ func main() {
 		return america[i].Load >= america[j].Load
 	})
 
+	sort.Slice(asia, func(i, j int) bool {
+		return asia[i].Load >= asia[j].Load
+	})
+
+	sort.Slice(oceania, func(i, j int) bool {
+		return oceania[i].Load >= oceania[j].Load
+	})
+
+	header:="server name     cc  city      bandwidth users load  stat  vpn name"
+
 	fmt.Println("+ america")
+	fmt.Println(header)
 	for _, s := range america {
 		printServer(s)
 	}
 
 	fmt.Println("\n+ europe")
+	fmt.Println(header)
 	for _, s := range eu {
+		printServer(s)
+	}
+
+	fmt.Println("\n+ asia")
+	fmt.Println(header)
+	for _, s := range asia {
+		printServer(s)
+	}
+
+	fmt.Println("\n+ oceania")
+	fmt.Println(header)
+	for _, s := range oceania {
+		printServer(s)
+	}
+
+	fmt.Println("\n+ all")
+	fmt.Println(header)
+	for _, s := range all {
 		printServer(s)
 	}
 }
 
 func printServer(s serverT) {
 	fmt.Printf("%-16s%s  %-13s", s.Name, s.CountryCode, s.Location)
-	fmt.Printf("%7d %5d %4d", s.FreeBW, s.Users, s.Load)
-	fmt.Printf("%8s\n", s.Health)
+	fmt.Printf("%6d %5d %4d", s.FreeBW, s.Users, s.Load)
+	fmt.Printf("%6s  ", s.Health)
+	fmt.Printf("%s-%s\n", s.CountryCode, str.ToLower(s.Name))
 }
